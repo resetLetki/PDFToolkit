700a9ac8fab5d017054f0d1688d96d2dc51a4d43
OlesenJonas <45714731+OlesenJonas@users.noreply.github.com>
2019-11-20 18:54:53 +0100

    Update PDFMerger.py

--- PDFMerger.py
+++ PDFMerger.py
@@ -1,5 +1,5 @@
 missing = []
-from sys import argv
+import os
 try:    
     from PyPDF2 import PdfFileMerger
 except:
@@ -15,25 +15,54 @@ for mod in missing:
 
 del missing 
 
-###############################################
+############################################## Warning: No error handling
+############################################## actual functionality #######################################
+
+def create_pdf():
+    pass
+
+def merge(file1,file2,out):
+    base_p = os.path.split(file1)[0]
+
+    merger = PdfFileMerger()
+
+    #file1 = file1.replace("\\","\\\\")
+    #file2 = file2.replace("\\","\\\\")
+    merger.append(file1)
+    merger.append(file2)
+
+    # Write to an output PDF document
+    output = open(base_p+"/"+out+".pdf", "wb")
+    merger.write(output)
+
+def processPDFs(vals):
+    if(vals["file_2_k"]!=""):
+        merge(vals["file_1_k"],vals["file_2_k"],vals["out_k"])
+
+    if(vals["Deckb_k"]):
+        print("deckblatt wird generiert!")
+        #TODO: create and merge with end
+    
+
+############################################### UI ######################################################
 
 layout = [
     [sg.Text("Deckblatt erstellen")],
     [sg.Text("Kurs")],
-    [sg.Input()],
+    [sg.Input(key="kurs_k")],
     [sg.Text("Gruppe")],
-    [sg.Input()],
+    [sg.Input(key="gruppe_k")],
     [sg.Text("Blatt")],
-    [sg.Input()],
+    [sg.Input(key="blatt_k")],
     [sg.Text("Mitglieder")],
-    [sg.Input()],
+    [sg.Input(key="mitgl_k")],
     [sg.Text('PDF File')], 
-    [sg.Input(), sg.FileBrowse()],
+    [sg.Input(key="file_1_k"), sg.FileBrowse()],
     [sg.Text('Optional File 2')], 
-    [sg.Input(), sg.FileBrowse()],
+    [sg.Input(key="file_2_k"), sg.FileBrowse()],
     [sg.Text("Output filename")],
-    [sg.Input()],
-    [sg.Button("Create")]
+    [sg.Input(key="out_k")],
+    [sg.Checkbox("Deckblatt generieren",default=True,key="Deckb_k"),sg.Button("Process")]
 ]
 
 window = sg.Window("PDF Toolkit",layout)
@@ -41,25 +70,11 @@ window = sg.Window("PDF Toolkit",layout)
 while(True):
     print()
     event, values = window.read()
-    print(event,values)
+    if(event == "Process"):
+        processPDFs(values)
     if event in (None, "Cancel"):
         break
 
-window.close(); 
+window.close()
 
-def create_pdf():
-    pass
-
-def merge():
-    args = argv
-
-    merger = PdfFileMerger()
-
-    for arg in args[1:-1]:
-        file_p = arg.replace("\\","\\\\")
-        print(file_p)
-        merger.append(file_p)
-
-    # Write to an output PDF document
-    output = open(args[-1]+".pdf", "wb")
-    merger.write(output)
+#################################################### END OF UI STUFF #################################
